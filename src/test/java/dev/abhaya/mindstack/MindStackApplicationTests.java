4d6345c44b19a92962472191778b054704443d96
AbhayPratap <pratapabhayedu22@gmail.com>
2026-02-10 18:58:01 +0530

    init: MindStack Backend Structure (v0.00)

---
+++ src/test/java/dev/abhaya/mindstack/MindStackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abhaya.mindstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
